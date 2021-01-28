4cd5457e2ecb0b5f1236d36b774592bda8112bd0
James Jongyoon Han <jameshan@Jamess-MacBook-Pro.local>
2021-01-28 06:30:58 -0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cjapp/services/app_colors.dart';
 
-
+// optimization
 
 const String testDevice = 'Mobile_id';
 
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -42,7 +42,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     WidgetsBinding.instance.addObserver(
         LifecycleEventHandler(resumeCallBack: () async => setStateIfMounted(checkPermissions()))
     );
-    _tabController = TabController(vsync: this, length: 5);
+    _tabController = TabController(vsync: this, length: 4);
   }
 
    setStateIfMounted(Future<void> f) {
@@ -142,7 +142,6 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
               initSortBy: sortBy,
               setInit: setInitVariablesFeed,
             ),
-            ThePad(),
             SearchPage(),
             _auth.currentUser == null ? PleaseSignIn() : ProfilePage(),
             MapPage(),
@@ -173,11 +172,11 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                 color: Colors.black,
               ),
             ),
-            Tab(
-              icon: Icon(Icons.auto_awesome,
-                color: Colors.black,
-
-            ),),
+            // Tab(
+            //   icon: Icon(Icons.auto_awesome,
+            //     color: Colors.black,
+            //
+            // ),),
             Tab(
               icon: Icon(
                 Icons.search,
@@ -188,12 +187,6 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
               icon: Icon(
                 Icons.person,
                 color: Colors.black,
-              ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.error,
-                color: Colors.red,
               ),
             ),
             Tab(
